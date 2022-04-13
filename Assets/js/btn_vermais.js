73d7c4623f359e634589115c7e8caf5876e4c218
Gustavo <gustavopereirafacal@gmail.com>
2022-04-12 22:29:10 -0300

    alterações no botao

--- Assets/js/btn_vermais.js
+++ Assets/js/btn_vermais.js
@@ -1,4 +1,4 @@
-const btnVermais = document.querySelector('.ver_mais-btn');
+const btnVermais = document.querySelectora('.ver_mais-btn');
 
 btnVermais.addEventListener('mousemove', function(e){
     const x = e.pageX - btnVermais.offsetLeft;
